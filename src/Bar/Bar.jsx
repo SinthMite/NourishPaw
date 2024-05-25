ccac7215ee48d6b73c9f0af254d2510771de7c95
Sinth <sinthmite@gmail.com>
2024-05-24 23:42:14 -0400

    Menu Fixed

--- src/Bar/Bar.jsx
+++ src/Bar/Bar.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import Paw from '../assets/dog-paw.svg';
 import './Bar.scss';
 
 export default function Bar({ logInState }) {
-    const { isOpen, setIsOpen, toggle, loggedIn, setLoggedIn } = logInState;
+    const { isOpen, toggle, loggedIn } = logInState;
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <div className="bar">
-            <div>
+            <div className="bar-header">
                 <img src={Paw} alt="paw" className="pawLogo" />
+                <button className="menu-toggle" onClick={handleMenuToggle}>
+                    ☰
+                </button>
             </div>
-            <div className="bar-list">
+            <div className={`bar-list ${menuOpen ? 'open' : ''}`}>
                 <ul>
                     <li><Link to="/mypets">My Pets</Link></li>
                     <li><Link to="/mealplan">Meal Planner</Link></li>
                     <li><Link to="/tracker">Nutrition Tracker</Link></li>
                     <li><Link to="/resources">Resources</Link></li>
                 </ul>
-                <ul>
-                    <li><button>My Account</button></li>
-                    {loggedIn ? (
-                        <li><button onClick={toggle}>Log Out</button></li>
-                    ) : (
-                        <li><button onClick={toggle}>Log In</button></li>
-                    )}
-                </ul>
+            </div>
+            <div className="account-buttons">
+                <button>My Account</button>
+                {loggedIn ? (
+                    <button onClick={toggle}>Log Out</button>
+                ) : (
+                    <button onClick={toggle}>Log In</button>
+                )}
             </div>
         </div>
     );
